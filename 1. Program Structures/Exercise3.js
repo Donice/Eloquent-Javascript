823728da45722a7a01998b420f05b6a1e1149c03
Donice Ubaru <doniceubaru1@gmail.com>
2022-06-19 21:52:07 +0100

    finished chapter 1

---
+++ 1. Program Structures/Exercise3.js
@@ -0,0 +1,16 @@
+// CHESSBOARD
+
+let chessboard = "";
+let grid = 8
+
+for(let i = 0; i < grid; i++ ) {
+   for(let j = 0; j < grid; j++) {
+      if((i + j) % 2 === 0) {
+         chessboard += " "
+      } else {
+         chessboard += "#"
+      }
+   }
+   chessboard += '\n'
+}
+console.log(chessboard)
