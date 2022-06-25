e8fd3076be6a537dd1c68619b3e6ab28e7e8ea59
Donice Ubaru <doniceubaru1@gmail.com>
2022-06-25 01:43:16 +0100

    Started chapter 2

---
+++ 2. Functions/Excercise1.js
@@ -0,0 +1,5 @@
+function smalletsNumber(a, b) {
+   return Math.min(a, b)
+}
+
+console.log(smalletsNumber(10, 12))
---
+++ 2. Functions/Note.js
@@ -0,0 +1,120 @@
+const hummus = function (factor) {
+  const ingredient = function (amount, unit, name) {
+    let ingredientAmount = amount * factor;
+    if (ingredientAmount > 1) {
+      unit += "s";
+    }
+    // console.log(`${ingredientAmount} ${unit} ${name}`)
+  };
+  ingredient(1, "can", "chickpeas");
+  ingredient(0.25, "can", "tahini");
+  ingredient(0.25, "can", "lemon juice");
+  ingredient(1, "clove", "garlic");
+  ingredient(1, "tablespoon", "olive oil");
+  ingredient(1, "teaspoon", "cumin");
+};
+
+function wrapValue(n) {
+  let local = n;
+  return () => local;
+}
+
+let wrap1 = wrapValue(1);
+let wrap2 = wrapValue(2);
+
+// console.log(wrap1())
+
+function multiplier(factor) {
+  return (number) => number * factor;
+}
+
+let twice = multiplier(2);
+// console.log(twice(5))
+// -> 10
+
+function divide(dividend) {
+  return (divider) => divider / dividend;
+}
+
+let product = divide(20);
+// console.log(product(1200))
+
+function power(base, exponent) {
+  if (exponent == 0) {
+    return 1;
+  } else {
+    return base * power(base, exponent - 1);
+  }
+}
+// first iteration
+// say (10, 3)
+// since exponent is not equals to 0, we use else
+
+// console.log(power(2, 3))
+
+function findSolution(target) {
+  function find(current, history) {
+    if (current == target) {
+      return history;
+    } else if (current > target) {
+      return null;
+    } else {
+      return (
+        find(current + 5, `(${history} + 5)`) ||
+        find(current * 3, `(${history} * 3)`)
+      );
+    }
+  }
+  return find(1, "1");
+}
+
+// console.log(findSolution(11))
+
+//.........................................................
+
+function printFarmInventory(cows, chicken) {
+  let cowString = String(cows);
+  while (cowString.length < 3) {
+    cowString = "0" + cowString;
+  }
+  console.log(`${cowString} Cows`);
+  let chickenString = String(chicken);
+  while (chickenString.length < 3) {
+    chickenString = "0" + chickenString;
+  }
+  console.log(`${chickenString} Chickens`);
+}
+// printFarmInventory(67, 99)
+
+// ..........................................................
+
+function printZeroPaddedWithLabel(number, label) {
+  let numberString = String(number);
+  while (numberString.length < 3) {
+    numberString = "0" + numberString;
+  }
+  console.log(`${numberString} ${label}`);
+}
+
+function printFarmInventory2(cows, chickens, pigs) {
+  printZeroPaddedWithLabel(cows, "Cows");
+  printZeroPaddedWithLabel(chickens, "Chickens");
+  printZeroPaddedWithLabel(pigs, "Pigs");
+}
+
+// ...........................................................
+
+function zeroPad(number, width) {
+  let string = String(number);
+  while (string.length < width) {
+    string = "0" + string;
+  }
+  return string;
+}
+function printFarmInventory3(cows, chickens, pigs) {
+  console.log(`${zeroPad(cows, 3)} Cows`);
+  console.log(`${zeroPad(chickens, 3)} Chickens`);
+  console.log(`${zeroPad(pigs, 3)} Pigs`);
+}
+
+printFarmInventory3(29, 32, 108);
