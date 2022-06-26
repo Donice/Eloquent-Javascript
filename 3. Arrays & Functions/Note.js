01d69ab5cb8804eace506fde09a0aa5d25ad05fa
Donice Ubaru <doniceubaru1@gmail.com>
2022-06-27 00:12:01 +0100

    updated chapter 3

--- 3. Arrays & Functions/Note.js
+++ 3. Arrays & Functions/Note.js
@@ -58,8 +58,46 @@ function addEntry(events, squirrel) {
 }
 
 addEntry(["work", "touched tree", "pizza", "running", "televison"], false);
-addEntry(["work", "ice cream", "cauliflower", "lasagna", "touched tree"],false)
-addEntry(["weekend", "cycling", "break", "peanuts", "bear"], true)
+addEntry(
+  ["work", "ice cream", "cauliflower", "lasagna", "touched tree"],
+  false
+);
+addEntry(["weekend", "cycling", "break", "peanuts", "bear"], true);
 
-console.log(journal)
+// console.log(journal)
 
+// .........................................
+
+const table = [76, 9, 4, 1];
+
+function phi(table) {
+  return (
+    (table[3] * table[0] - table[2] * table[1]) /
+    Math.sqrt(
+      (table[2] + table[3]) *
+        (table[0] + table[1]) *
+        (table[1] + table[3]) *
+        (table[0] + table[2])
+    )
+  );
+}
+
+// console.log(phi(table))
+
+//................................................
+
+const JOURNAL = require('./JOURNAL')
+
+function tableFor(event, journal) {
+  let table = [0, 0, 0, 0];
+  for (let i = 0; i < journal.length; i++) {
+    let entry = journal[i],
+      index = 0;
+    if (entry.events.includes(event)) index += 1;
+    if (entry.squirrel) index += 2;
+    table[index] += 1;
+  }
+  return table;
+}
+
+console.log(tableFor("carrot", JOURNAL));
