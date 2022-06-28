dddbc3aab66de59ffe43cabf1741078934317ec9
Donice Ubaru <doniceubaru1@gmail.com>
2022-06-28 05:14:29 +0100

    concluded excercise 1

--- 3. Arrays & Functions/Excercise1.js
+++ 3. Arrays & Functions/Excercise1.js
@@ -1,13 +1,29 @@
 // THE SUM OF A RANGE
 
-function range(a, b) {
-  let sum = 0;
-  while (a <= b) {
-    sum += a;
-    a++;
-  }
 
+function range(a, b, step = 1) {
+  let sum = [];
+  let inc = Math.abs(step)
+
+  while (a <= b) {
+    sum.push(a)
+    a += inc
+  }
+  while (a >= b) {
+   sum.push(a)
+   a -= inc
+  } 
   return console.log(sum);
 }
+range(1, 10, 2)
+range(10, 1, -2);
 
-range(1, 15);
+
+function sum(array) {
+   let sum = 0
+   for(let i of array) {
+      sum += i
+   }
+   return console.log(sum);
+}
+sum([10, 2, 8, 5]);
